8911187fde780a66efadfa53158cd8857b20eb0a
Darth Vader <flytoshanjai@gmail.com>
2023-06-24 20:36:10 +0530

    first commit

---
+++ booklib-app-using-react-js-and-openlib-api-master/src/pages/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./About.css";
+import aboutImg from "../../images/about-img.jpg";
+
+const About = () => {
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src = {aboutImg} alt = "" />
+          </div>
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About Alpha Books</h2>
+            <p className='fs-17'>Books are keys that unlock the doors to new worlds and endless possibilities.In the realm of books, curious minds find nourishment for the soul.The written word is an eternal flame, illuminating the path of enlightenment.<br /><br /><strong>"Books are companions that never fail to engage, entertain, and educate."</strong></p>
+            <p className='fs-17'>At Alpha Books, we are passionate about igniting the love for reading and the pursuit of knowledge. We believe that books have the power to transport, inspire, and transform lives. We are dedicated to curating an exceptional collection of books that cater to diverse interests and genres, ensuring that there is something for everyone within our literary sanctuary. Our mission is to create a haven for book enthusiasts, a place where they can explore new worlds, embark on captivating adventures, and delve into the depths of imagination. With a commitment to quality, we meticulously select each title, offering a handpicked selection of timeless classics, contemporary bestsellers, thought-provoking non-fiction, and much more. </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
